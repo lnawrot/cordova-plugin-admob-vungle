1c66075280c7f680e64d65c8eac50cf578432b19
Łukasz Nawrot <wookash@wookash.net>
2021-05-05 10:32:02 +0200

    update vungle to work with AdmobSDK 8

--- src/ios/Vungle.m
+++ src/ios/Vungle.m
@@ -4,16 +4,9 @@
 #import <VungleSDK/VungleSDK.h>
 #import <VungleAdapter/VungleAdapter.h>
 #import <VungleAdapter/VungleAdNetworkExtras.h>
+
 #import <GoogleMobileAds/GADExtras.h>
-
-#import <GoogleMobileAds/GADInterstitial.h>
-#import <GoogleMobileAds/GADInterstitialDelegate.h>
-
-@interface VunglePlugin () <GADInterstitialDelegate>
-
-@property(nonatomic, strong) GADInterstitial *interstitial;
-
-@end
+#import <GoogleMobileAds/GADRewardedAd.h>
 
 @implementation VunglePlugin
 
@@ -22,18 +15,22 @@
     if (self) {
         NSDictionary* settings = self.commandDelegate.settings;
         NSString* rewardvideoId = settings[@"REWARDVIDEO_ID"];
-        NSString* interstitialId = settings[@"INTERSTITIAL_ID"];
         NSLog(@"Vungle mediation plugin init");
         NSLog(@"rewardvideoId");
 
         GADRequest *request = [GADRequest request];
         VungleAdNetworkExtras *extras = [[VungleAdNetworkExtras alloc] init];
 
-        NSMutableArray *placements = [[NSMutableArray
-            alloc]initWithObjects:rewardvideoId, nil];
+        NSMutableArray *placements = [[NSMutableArray alloc]initWithObjects:rewardvideoId, nil];
         extras.allPlacements = placements;
         [request registerAdNetworkExtras:extras];
 
-        [[GADRewardBasedVideoAd sharedInstance] loadRequest:[GADRequest request] withAdUnitID: rewardvideoId];
+        [GADRewardedAd
+            loadWithAdUnitId:rewardvideoId
+            request:request
+            completionHandler:^(GADRewarded *ad, NSError *error) {
+                NSLog(@"load handler");
+            }
+        ];
     }
 } @end
