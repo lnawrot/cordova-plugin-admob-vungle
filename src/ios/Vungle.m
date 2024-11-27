34468c5d238cec4922896c538747d9b9126cb49a
Łukasz Nawrot <wookash@wookash.net>
2024-11-27 10:53:55 +0100

    update vungle sdk

--- src/ios/Vungle.m
+++ src/ios/Vungle.m
@@ -1,36 +1,8 @@
 #import <Cordova/CDV.h>
 #import "Vungle.h"
 
-#import <VungleSDK/VungleSDK.h>
-#import <VungleAdapter/VungleAdapter.h>
-#import <VungleAdapter/VungleAdNetworkExtras.h>
-
-#import <GoogleMobileAds/GADExtras.h>
-#import <GoogleMobileAds/GADRewardedAd.h>
-
 @implementation VunglePlugin
 
 - (void)pluginInitialize {
     [super pluginInitialize];
-    if (self) {
-        NSDictionary* settings = self.commandDelegate.settings;
-        NSString* rewardvideoId = settings[@"REWARDVIDEO_ID"];
-        NSLog(@"Vungle mediation plugin init");
-        NSLog(@"rewardvideoId");
-
-        GADRequest *request = [GADRequest request];
-        VungleAdNetworkExtras *extras = [[VungleAdNetworkExtras alloc] init];
-
-        NSMutableArray *placements = [[NSMutableArray alloc]initWithObjects:rewardvideoId, nil];
-        extras.allPlacements = placements;
-        [request registerAdNetworkExtras:extras];
-
-        [GADRewardedAd
-            loadWithAdUnitID:rewardvideoId
-            request:request
-            completionHandler:^(GADRewardedAd *ad, NSError *error) {
-                NSLog(@"load handler");
-            }
-        ];
-    }
 } @end
